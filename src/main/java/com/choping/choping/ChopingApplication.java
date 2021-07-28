b19562dc42667277a211879b127aab34f0ced1c0
PepeBeto-code <josewavves@hotmail.com>
2021-07-28 15:50:20 -0500

    primer commit

---
+++ src/main/java/com/choping/choping/ChopingApplication.java
@@ -0,0 +1,13 @@
+package com.choping.choping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChopingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChopingApplication.class, args);
+	}
+
+}
